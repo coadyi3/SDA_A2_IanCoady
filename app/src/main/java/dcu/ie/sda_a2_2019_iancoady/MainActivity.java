c0287e9f7b80a02663908ee6a7b39a88bc3dc313
coadyi3 <ian.coady3@mail.dcu.ie>
2019-11-04 10:11:09 +0000

    Project Created

---
+++ app/src/main/java/dcu/ie/sda_a2_2019_iancoady/MainActivity.java
@@ -0,0 +1,14 @@
+package dcu.ie.sda_a2_2019_iancoady;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
