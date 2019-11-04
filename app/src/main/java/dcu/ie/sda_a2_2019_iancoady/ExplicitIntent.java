e09155ae3ed7154e08b0328e8414aeefd98b2455
coadyi3 <ian.coady3@mail.dcu.ie>
2019-11-04 16:10:48 +0000

    Added return data functionality and Gmail functionality.

--- app/src/main/java/dcu/ie/sda_a2_2019_iancoady/ExplicitIntent.java
+++ app/src/main/java/dcu/ie/sda_a2_2019_iancoady/ExplicitIntent.java
@@ -2,13 +2,42 @@ package dcu.ie.sda_a2_2019_iancoady;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ExplicitIntent extends AppCompatActivity {
+
+    private Button saveButton;
+    private EditText email;
+    private EditText subject;
+    private EditText content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.explicit_intent);
+
+        saveButton = (Button)findViewById(R.id.saveButton);
+        email = (EditText)findViewById(R.id.emailEditTest);
+        subject = (EditText)findViewById(R.id.subjectEditText);
+        content = (EditText) findViewById(R.id.composeMSg);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Ref: https://www.javatpoint.com/android-explicit-intent-example
+                Intent returnToMain = new Intent(getApplicationContext(), MainActivity.class);
+                returnToMain.putExtra("Email", email.getText().toString());
+                returnToMain.putExtra("Subject", subject.getText().toString());
+                returnToMain.putExtra("Content", content.getText().toString());
+
+                startActivity(returnToMain);
+            }
+        });
     }
 }
--- app/src/main/java/dcu/ie/sda_a2_2019_iancoady/MainActivity.java
+++ app/src/main/java/dcu/ie/sda_a2_2019_iancoady/MainActivity.java
@@ -3,16 +3,20 @@ package dcu.ie.sda_a2_2019_iancoady;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     static final    int         REQUEST_IMAGE_CAPTURE = 1;
-    private         TextView    callExplicit;
+    private         TextView    callExplicitButton;
+    private TextView emailDetails;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +24,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        callExplicit = (TextView)findViewById(R.id.openActivityIntent);
-        callExplicit    .setOnClickListener(new View.OnClickListener() {
+        emailDetails = findViewById(R.id.emailDetails);
+        setContentView(R.layout.activity_main);
+        final Bundle details = getIntent().getExtras();
+        if(details == null){
+            Log.i(TAG, "No details to print");
+        }
+        else {
+            String email = details.getString("Email", "ian.coady3@mail.dcu.ie");
+
+            Log.i(TAG, email); //Log msg to ensure the values we're being passed from the other activity correctly
+
+            String subject = details.getString("Subject", "SDA Email");
+            String content = details.getString("Content", "Hello World");
+            String textView = String.format("%s\n%s\n%s", email, subject, content);
+            emailDetails.append(textView);
+            Log.i(TAG, emailDetails.getText().toString());
+        }
+
+        callExplicitButton = findViewById(R.id.openActivityIntent);
+        callExplicitButton    .setOnClickListener(new View.OnClickListener() {
             @Override
             //Demonstrating the use of onClickListener as opposed to XML onClick
             public void onClick(View v) {
@@ -30,8 +52,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if(details == null){
+                    Log.i(TAG, "No details to print");
+                }
+                else {
+                    Intent intent = new Intent(Intent.ACTION_SENDTO);
+                    intent.putExtra(Intent.EXTRA_SUBJECT, details.getString("Subject"));
+                    intent.putExtra(Intent.EXTRA_TEXT, details.getString("Content"));
+                    intent.setData(Uri.parse("mailto:" +details.getString("Email")));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+
+            }
+        }));
     }
+
 
     public void openCamera(View view) {
 
