f81a5ed07ef063ecef56eb84899f48efb2df8389
coadyi3 <ian.coady3@mail.dcu.ie>
2019-11-04 12:02:40 +0000

    Gallery Functionality added.

--- app/src/main/java/dcu/ie/sda_a2_2019_iancoady/MainActivity.java
+++ app/src/main/java/dcu/ie/sda_a2_2019_iancoady/MainActivity.java
@@ -18,9 +18,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openCamera(View view) {
+
+        //Ref: https://developer.android.com/training/camera/photobasics
         Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(takePhoto.resolveActivity(getPackageManager()) != null){
             startActivityForResult(takePhoto,REQUEST_IMAGE_CAPTURE);
         }
+    }
+
+    public void openGallery(View view) {
+
+        //Ref: https://stackoverflow.com/questions/16928727/open-gallery-app-from-android-intent
+        Intent openPhotos = new Intent();
+        openPhotos.setAction(android.content.Intent.ACTION_VIEW);
+        openPhotos.setType("image/*");
+        openPhotos.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(openPhotos);
     }
 }
