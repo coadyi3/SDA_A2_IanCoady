e76a295f22e63f57e0460c8b8a598876fb250a6d
coadyi3 <ian.coady3@mail.dcu.ie>
2019-11-04 11:23:49 +0000

    Camera functionality added

--- app/src/main/java/dcu/ie/sda_a2_2019_iancoady/MainActivity.java
+++ app/src/main/java/dcu/ie/sda_a2_2019_iancoady/MainActivity.java
@@ -2,13 +2,25 @@ package dcu.ie.sda_a2_2019_iancoady;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openCamera(View view) {
+        Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePhoto.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePhoto,REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
