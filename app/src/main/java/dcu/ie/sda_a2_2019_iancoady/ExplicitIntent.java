a5331b95ebad54866db512ad82f76286f1934660
coadyi3 <ian.coady3@mail.dcu.ie>
2019-11-04 12:48:27 +0000

    OnClick listener intent and Explicit activity GUI created.

---
+++ app/src/main/java/dcu/ie/sda_a2_2019_iancoady/ExplicitIntent.java
@@ -0,0 +1,14 @@
+package dcu.ie.sda_a2_2019_iancoady;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExplicitIntent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.explicit_intent);
+    }
+}
--- app/src/main/java/dcu/ie/sda_a2_2019_iancoady/MainActivity.java
+++ app/src/main/java/dcu/ie/sda_a2_2019_iancoady/MainActivity.java
@@ -6,15 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final    int         REQUEST_IMAGE_CAPTURE = 1;
+    private         TextView    callExplicit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        callExplicit = (TextView)findViewById(R.id.openActivityIntent);
+        callExplicit    .setOnClickListener(new View.OnClickListener() {
+            @Override
+            //Demonstrating the use of onClickListener as opposed to XML onClick
+            public void onClick(View v) {
+                Intent openExplicit = new Intent(getApplicationContext(),ExplicitIntent.class);
+                startActivity(openExplicit);
+            }
+        });
+
+
     }
 
     public void openCamera(View view) {
